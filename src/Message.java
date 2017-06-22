7d655a48ddf71cf8012917acb361c0598779151b
Marco <kinglessqueenrium@gmail.com>
2017-06-22 21:56:19 +0200

    Reformatted the code

--- src/Message.java
+++ src/Message.java
@@ -4,36 +4,36 @@ import java.util.ArrayList;
 
 public class Message
 {
-    public enum QueryMethod
-    {
-	ITERATIVE, RECURSIVE
-    }
-    
-    public Header   header;
-    public Question question;
-    
-    public ArrayList<ResourceRecord> answers;
-    public ArrayList<ResourceRecord> authority;
-    public ArrayList<ResourceRecord> additional;    
-    
-    public Message()
-    {
-	header   = new Header();
-	question = new Question();
-	
-	answers    = new ArrayList<ResourceRecord>();
-	authority  = new ArrayList<ResourceRecord>();
-	additional = new ArrayList<ResourceRecord>();	
-    }
+  public enum QueryMethod
+  {
+    ITERATIVE, RECURSIVE
+  }
 
-    public class Header
-    {
-	public QueryMethod method = QueryMethod.ITERATIVE;
-    }
-    
-    public class Question
-    {
-	public String name;
-	public ResourceRecord.Type type;
-    }
+  public Header   header;
+  public Question question;
+
+  public ArrayList<ResourceRecord> answers;
+  public ArrayList<ResourceRecord> authority;
+  public ArrayList<ResourceRecord> additional;
+
+  public Message()
+  {
+    header   = new Header();
+    question = new Question();
+
+    answers    = new ArrayList<ResourceRecord>();
+    authority  = new ArrayList<ResourceRecord>();
+    additional = new ArrayList<ResourceRecord>();
+  }
+
+  public class Header
+  {
+    public QueryMethod method = QueryMethod.ITERATIVE;
+  }
+
+  public class Question
+  {
+    public String name;
+    public ResourceRecord.Type type;
+  }
 }
--- src/Resolver.java
+++ src/Resolver.java
@@ -12,111 +12,110 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Resolver
 {
-    private CacheServer server;
-    private ConcurrentHashMap<Integer, String> pendingRequests;
+  private CacheServer server;
+  private ConcurrentHashMap<Integer, String> pendingRequests;
 
-    private static final String NotSet = "NotSet";
+  private static final String NotSet = "NotSet";
 
-    private static int globalRequestCounter = 0;
+  private static int globalRequestCounter = 0;
 
-    private static final String LogDirectory  = "log/resolver";
-    private static final String LogPrefix     = "resolve_";
+  private static final String LogDirectory  = "log/resolver";
+  private static final String LogPrefix     = "resolve_";
 
-    public static final String LastFileWrote = ".resolverRequest";
+  public static final String LastFileWrote = ".resolverRequest";
 
-    public Resolver(CacheServer server)
+  public Resolver(CacheServer server)
+  {
+    this.server = server;
+    this.pendingRequests = new ConcurrentHashMap<Integer, String>();
+  }
+
+  public int ask(String domainAddress, RequestType what)
+  {
+    // Basically ping pong
+    // Search for A/AAAA until you get it
+    // When NS or CNAME are found handle them by asking the servers
+    int requestCode = globalRequestCounter++;
+    ConnectionHandler connection = new ConnectionHandler(requestCode, domainAddress, what);
+    connection.start();
+
+    return requestCode;
+  }
+
+  public String askAndWait(String domainAddress, RequestType what)
+  {
+    int requestCode = ask(domainAddress, what);
+
+    return getOrWait(requestCode);
+  }
+
+  public String getOrWait(int requestCode)
+  {
+
+    if(!pendingRequests.containsKey(requestCode))
+      return null;
+    System.out.println("Waiting...");
+    while(pendingRequests.get(requestCode).equals(NotSet));
+
+    String response = pendingRequests.get(requestCode);
+    pendingRequests.remove(requestCode);
+
+
+    return response;
+  }
+
+  private class ConnectionHandler extends Thread
+  {
+    private int         requestCode;
+    private String      domainAddress;
+    private RequestType what;
+
+    public ConnectionHandler(int requestCode, String domainAddress, RequestType what)
     {
-	this.server = server;
-	this.pendingRequests = new ConcurrentHashMap<Integer, String>();
+      this.domainAddress = domainAddress;
+      this.what          = what;
+      this.requestCode   = requestCode;
+      pendingRequests.put(requestCode, NotSet);
     }
 
-    public int ask(String domainAddress, RequestType what)
+    public void run()
     {
-	// Basically ping pong
-	// Search for A/AAAA until you get it
-	// When NS or CNAME are found handle them by asking the servers
-	int requestCode = globalRequestCounter++;
-	ConnectionHandler connection = new ConnectionHandler(requestCode, domainAddress, what);
-	connection.start();
+      long time = System.nanoTime();
 
-	return requestCode;
+      int responseCode        = server.query(domainAddress, what);
+      ResourceRecord response = server.getOrWait(responseCode);
+      pendingRequests.put(requestCode, response.rdata);
+
+      time = System.nanoTime() - time;
+      String filename = LogDirectory + "/" + LogPrefix + domainAddress.replaceAll("\\.", "") + ".txt";
+      writeLog(filename, response, time);
     }
 
-    public String askAndWait(String domainAddress, RequestType what)
+    private boolean writeLog(String filename, ResourceRecord response, long nanoTime)
     {
-	int requestCode = ask(domainAddress, what);
+      try
+      {
+        PrintWriter out = new PrintWriter(new FileOutputStream(filename));
 
-	return getOrWait(requestCode);
-    }
+        out.println("Starting exchange with the cache server");
+        out.println("-------------------------------------------------------");
+        out.println("Requested IP address of: " + domainAddress);
+        out.println("Cache server response: "   + response.rdata);
+        out.println("Time elapsed: " + (((double)nanoTime)/100000000));
 
-    public String getOrWait(int requestCode)
-    {
-
-	if(!pendingRequests.containsKey(requestCode))
-	    return null;
-	System.out.println("Waiting...");
-	while(pendingRequests.get(requestCode).equals(NotSet));
-
-	String response = pendingRequests.get(requestCode);
-	pendingRequests.remove(requestCode);
-
-
-	return response;
-    }
-
-    private class ConnectionHandler extends Thread
-    {
-	private int         requestCode;
-	private String      domainAddress;
-	private RequestType what;
-
-	public ConnectionHandler(int requestCode, String domainAddress, RequestType what)
-	{
-	    this.domainAddress = domainAddress;
-	    this.what          = what;
-	    this.requestCode   = requestCode;
-	    pendingRequests.put(requestCode, NotSet);
-	}
-
-	public void run()
-	{
-	    long time = System.nanoTime();
-
-	    int responseCode        = server.query(domainAddress, what);
-	    ResourceRecord response = server.getOrWait(responseCode);
-	    pendingRequests.put(requestCode, response.rdata);
-
-	    time = System.nanoTime() - time;
-	    String filename = LogDirectory + "/" + LogPrefix + domainAddress.replaceAll("\\.", "") + ".txt";
-	    if(!writeLog(filename, response, time))
-		System.out.println("Printed correctly");
-       	}
-
-	private boolean writeLog(String filename, ResourceRecord response, long nanoTime)
-	{
-	    try
-	    {
-    		PrintWriter out = new PrintWriter(new FileOutputStream(filename));
-
-    		out.println("Starting exchange with the cache server");
-    		out.println("-------------------------------------------------------");
-    		out.println("Requested IP address of: " + domainAddress);
-    		out.println("Cache server response: "   + response.rdata);
-    		out.println("Time elapsed: " + (((double)nanoTime)/100000000));
-
-    		out.close();
+        out.close();
 
         Files.copy(new File(filename).toPath(),
-                   new File(LastFileWrote).toPath(),
-                   StandardCopyOption.REPLACE_EXISTING);
+        new File(LastFileWrote).toPath(),
+        StandardCopyOption.REPLACE_EXISTING);
 
-    		return true;
-	    }
-	    catch(IOException ie)
-	    {
-		ie.printStackTrace();
-		return false;
-	    }
-	}
+        return true;
+      }
+      catch(IOException ie)
+      {
+        ie.printStackTrace();
+        return false;
+      }
     }
+  }
 }
